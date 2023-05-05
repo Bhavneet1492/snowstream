217802101c86fccc492cc011db185eefc43c1a83
Bhavneet Kaur <76656963+Bhavneet1492@users.noreply.github.com>
2023-05-05 13:38:34 +0530

    change map view location

--- pages/Stream.py
+++ pages/Stream.py
@@ -156,6 +156,6 @@ with tab4:
     df_new=df_new[['latitude','longitude']]
     df_new['name']=df["DISPLAY_NAME"]
     df_new.dropna(inplace=True)
-    map = folium.Map(location=[0, 0], zoom_start=2)
+    map = folium.Map(location=[50, 50], zoom_start=2)
     for i, row in df_new.iterrows():folium.Marker(location=[row['latitude'], row['longitude']], popup=row["name"]).add_to(map)
     st_folium(map,width=2000, height=500)
